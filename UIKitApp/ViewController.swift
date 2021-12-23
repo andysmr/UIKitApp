c7e4c45455f724c81a2ad2dec3c4d271d83f8cfe
andySmr <andrey180696007@yandex.ru>
2021-12-23 15:38:29 +0400

    second version

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var segmentedControll: UISegmentedControl!
+    @IBOutlet var slider: UISlider!
     @IBOutlet var mainLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        // segmentedControl
         segmentedControll.insertSegment(withTitle: "Third", at: 2, animated: false)
+        
+        //lable
         mainLable.text = ""
         mainLable.font = mainLable.font.withSize(30)
         mainLable.textAlignment = .center
         mainLable.numberOfLines = 2
+        
+        //slider
+        slider.value = 1
+        slider.minimumValue = 1
+        slider.maximumValue = 100
+        slider.minimumTrackTintColor = .yellow
+        slider.maximumTrackTintColor = .blue
+        slider.thumbTintColor = UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1)
         // Do any additional setup after loading the view.
     }
 
