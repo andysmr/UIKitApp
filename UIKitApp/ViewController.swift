22da63e19af55a57d7b3c6d05fb235cd2075934a
andySmr <andrey180696007@yandex.ru>
2021-12-23 15:13:42 +0400

    first version

--- UIKitApp/ViewController.swift
+++ UIKitApp/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var segmentedControll: UISegmentedControl!
+    @IBOutlet var mainLable: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
+        segmentedControll.insertSegment(withTitle: "Third", at: 2, animated: false)
+        mainLable.text = ""
+        mainLable.font = mainLable.font.withSize(30)
+        mainLable.textAlignment = .center
+        mainLable.numberOfLines = 2
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func segmentedControllAction(_ sender: Any) {
+        switch segmentedControll.selectedSegmentIndex {
+        case 0:
+            mainLable.text = "Выбран сегмент с индексом 0"
+            mainLable.textColor = .red
+        case 1:
+            mainLable.text = "Выбран сегмент с индексом 1"
+            mainLable.textColor = .yellow
+        case 2:
+            mainLable.text = "Выбран сегмент с индексом 2"
+            mainLable.textColor = .green
+        default:
+            break
+        }
+    }
 }
 
